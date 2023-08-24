7e4d25db9c1546cec97ee59b08355e24985dbbb4
mr.private <35048386+kprprivate@users.noreply.github.com>
2023-08-24 20:09:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from modules import *
 
+# brazilian pasting is fucking incredible
 if __name__ == "__main__":
     mouseMovement = mainFunction()
-
+    
     for i in range(4):
         mouseMovement.move(100, 0)
         time.sleep(0.1)
